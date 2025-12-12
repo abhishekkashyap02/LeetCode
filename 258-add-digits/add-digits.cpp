add114c3b466b1c37468f4e85d14b09b3adfc3e6
Abhishek Kashyap <147583957+abhishekkashyap02@users.noreply.github.com>
2025-12-12 19:01:30 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (98.10%) - LeetSync

---
+++ 258-add-digits/add-digits.cpp
@@ -0,0 +1,16 @@
+// Abhishek Kashyap
+
+class Solution {
+public:
+    int addDigits(int num) {
+        while ( num >= 10 ) {
+            int sum = 0;
+            while ( num > 0 ) {
+                sum = sum + ( num % 10 );
+                num = num / 10;
+            }
+            num = sum;
+        }
+        return num;
+    }
+};
