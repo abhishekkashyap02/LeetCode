966904cf592ce10b5b49a130eebc16c4aa13132e
Abhishek Kashyap <147583957+abhishekkashyap02@users.noreply.github.com>
2026-01-03 21:55:32 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (24.03%) - LeetSync

---
+++ 1737-maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxDepth(string s) {
+        int maxDepth = 0;
+        int currDepth = 0;
+        for(char c : s) {
+            if(c == '(') {
+                currDepth++;
+                maxDepth = max(maxDepth, currDepth);
+            }
+            else if(c == ')') {
+                currDepth--;
+            }
+        }
+        return maxDepth;
+    }
+};
