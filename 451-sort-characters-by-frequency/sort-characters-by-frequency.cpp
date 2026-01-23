cf4a1ea54336e136a32d6323d1e406a6ec6396b0
Abhishek Kashyap <147583957+abhishekkashyap02@users.noreply.github.com>
2026-01-23 22:54:01 +0530

    Time: 2 ms (62.52%) | Memory: 18.7 MB (5.02%) - LeetSync

---
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
@@ -0,0 +1,30 @@
+// Abhishek kashyap
+class Solution {
+public:
+    string frequencySort(string s) {
+
+        vector<int> freq(256, 0);
+        for (char c : s) {
+            freq[c]++;
+        }
+
+        vector<string> bucket(s.length() + 1);
+
+        for (int i = 0; i < 256; i++) {
+            int f = freq[i];
+            if (f > 0) {
+                bucket[f].push_back((char)i);
+            }
+        }
+
+        string result = "";
+
+        for (int f = s.length(); f > 0; f--) {
+            for (char c : bucket[f]) {
+                result.append(f, c);  
+            }
+        }
+
+        return result;
+    }
+};
