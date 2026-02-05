15eb89f697196c1fcb210f7c5246400c5861f198
Abhishek Kashyap <147583957+abhishekkashyap02@users.noreply.github.com>
2026-02-05 23:21:29 +0530

    Time: 0 ms (100.00%) | Memory: 11.4 MB (77.01%) - LeetSync

---
+++ 66-plus-one/plus-one.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+
+        int n = digits.size();
+
+        // start from last digit
+        for (int i = n - 1; i >= 0; i--) {
+
+            // if digit is less than 9
+            if (digits[i] < 9) {
+                digits[i] = digits[i] + 1;
+                return digits;
+            }
+
+            // if digit is 9
+            digits[i] = 0;
+        }
+
+        // if all digits were 9
+        vector<int> result;
+        result.push_back(1);
+
+        for (int i = 0; i < n; i++) {
+            result.push_back(0);
+        }
+
+        return result;
+    }
+};
